c15f245692cf396a8c36fcccc8a84d4a253d3e0c
Александар Стефановић <stefanovicsalex@gmail.com>
2022-04-28 15:52:39 +0200

    Креирање ,,Стор процедура''

--- Упит.sql
+++ Упит.sql
@@ -1,5 +1,9 @@
+-- Kreiranje baze podataka:
+
 CREATE DATABASE Rent_a_Car;
 USE Rent_a_Car;
+
+-- Kreiranje tabela:
 
 CREATE TABLE Automobil(
   id INT PRIMARY KEY IDENTITY(1, 1),
@@ -38,3 +42,74 @@ CREATE TABLE Pozajmica(
   broj_dana INT NOT NULL,
   obavljeno NVARCHAR(2) NOT NULL
 );
+
+-- Strani kljucevi su vec definisani, zbog toga necu raditi ALTER TABLE naredbe!
+
+-- Kreiranje stor procedura:
+
+CREATE PROCEDURE Korisnik_Email
+@ime NVARCHAR(50),
+@prezime NVARCHAR(100),
+@email NVARCHAR(100),
+@lozinka NVARCHAR(50)
+AS
+SET LOCK_TIMEOUT 3000;
+BEGIN TRY
+  IF EXISTS(SELECT TOP 1 email FROM Korisnik
+            WHERE (ime = @ime) AND (prezime = @prezime) AND (email = @email) AND (lozinka = @lozinka))            
+  BEGIN
+    RETURN 0;
+  END
+  RETURN 1;
+END TRY
+BEGIN CATCH
+  RETURN @@ERROR;
+END CATCH;
+
+CREATE PROCEDURE Korisnik_Insert
+@ime NVARCHAR(50),
+@prezime NVARCHAR(100),
+@email NVARCHAR(100),
+@lozinka NVARCHAR(50),
+@uloga INT
+AS
+SET LOCK_TIMEOUT 3000;
+BEGIN TRY
+  IF EXISTS(SELECT TOP 1 email FROM Korisnik
+            WHERE email = @email)
+  RETURN 1
+  ELSE
+    INSERT INTO Korisnik (ime, prezime, email, lozinka, uloga) VALUES (@ime, @prezime, @email, @lozinka, @uloga)
+  RETURN 0;
+END TRY
+BEGIN CATCH
+  RETURN @@ERROR;
+END CATCH;
+
+CREATE PROCEDURE Korisnik_Update
+@ime NVARCHAR(50),
+@prezime NVARCHAR(100),
+@email NVARCHAR(100),
+@lozinka NVARCHAR(50),
+@uloga INT
+AS
+SET LOCK_TIMEOUT 3000;
+BEGIN TRY
+  IF EXISTS(SELECT TOP 1 email FROM Korisnik
+            WHERE email = @email)
+  BEGIN
+    UPDATE Korisnik SET
+    ime = @ime,
+    prezime = @prezime,
+    email = @email,
+    lozinka = @lozinka,
+    uloga = @uloga
+    WHERE email = @email;
+    
+    RETURN 0;
+  END;
+  RETURN -1;
+END TRY
+BEGIN CATCH
+  RETURN @@ERROR;
+END CATCH;
